a4f140b5e5c38a97c21d090fbbc7f2fdb9c6d28a
siddharth_dumbre <dumbre.si@northeastern.edu>
2023-11-04 18:16:44 -0400

    Button click and expression calculation

--- js/calc.js
+++ js/calc.js
@@ -13,25 +13,134 @@ class Button{
         this.#color=color;
     }
 
-    drawButton(text) {
+    drawButton(text,container) {
         const canvas=document.createElement("canvas");
-        document.body.appendChild(canvas);
+        canvas.setAttribute("height",""+this.#height*2);
+        canvas.setAttribute("width",""+this.#width*2);
+        container.appendChild(canvas);
         const ctx= canvas.getContext("2d");
-        ctx.rect(this.#startwidth,this.#startheight,this.#width,this.#height);
+        ctx.rect(10,this.#startheight,this.#width,this.#height);
         ctx.stroke();
-        ctx.fillText(text,this.#startwidth+20,this.#startheight+20);
-        // ctx.fillStyle(this.#color);
+        ctx.fillText(text,25,30);
+        ctx.fillStyle=this.#color;
+        if(text==="="){
+            evaluateButton=canvas;
+        }
+        canvas.addEventListener("click",(event)=>{
+            const target=event.target;
+
+            if(text==="back"){
+                expression=expression.substring(0,expression.length-1);
+                screen.innerHTML=expression;
+            }
+            if(text!="=" && text!="back"){
+                expression+=text;
+                screen.innerHTML=expression;
+            }
+            
+           
+        });
     }
 }
 
-// const one = document.getElementById("one");
-// const oneCtx= one.getContext("2d");
 
-// oneCtx.fillStyle= "blue";
-// oneCtx.fillText("1",100,100);
-// oneCtx.fillRect(10,10,20,20);
-for(let i=0;i<9;i++){
-    const a= new Button(10*i,10*i,40,40,"red");
-    a.drawButton(""+i);
+
+const container=document.querySelector(".container");
+//made a parent div container for holding all buttons
+const container1=document.getElementById("container-1");
+// Each customized container will hold all buttons of a single line
+const container2=document.getElementById("container-2");
+const container3=document.getElementById("container-3");
+const container4=document.getElementById("container-4");
+const container5=document.getElementById("container-5");
+
+const containers=[container1,container2,container3,container4,container5];
+
+
+const screen =document.getElementById("screen");
+const answer=document.getElementById("answer");
+var expression="";
+
+
+let space=0;
+
+var evaluateButton;
+
+var buttons=["","","","%","/","(","7","8","9","*",")","4","5","6","-","back","1","2","3","+","","","0",".","="];
+let btn_count=0;
+for(let i=0;i<5;i++){
+    space=0;
+    for(let j=0;j<5;j++){
+        var btn=new Button(10*space,10,40,40,"#9933CC");
+        btn.drawButton(buttons[btn_count],containers[i]);
+        space++;
+        btn_count++;
+    }
 }
+
+evaluateButton.addEventListener("click",()=>{
+    // eval(expression);
+    answer.innerHTML=eval(expression);
+});
+
+
+// for(let i=0;i<5;i++){
+//     const a= new Button(10*i,10,40,40,'#9933CC');
+//     a.drawButton(""+i,container1);
+// }
+// const back="back";
+// const bracket_open="(";
+// const bracket_close=")";
+// const equals= "=";
+// const percent="%";
+// const divide="/";
+// const multiply="X";
+// const add="+";
+// const minus="-";
+// const point=".";
+// space=0;
+// const back_button= new Button(10*space,10,40,40,"#9933CC");
+// back_button.drawButton(back,container3);
+// space++;
+// const bracket_open_btn= new Button(10*space,10,40,40,"#9933CC");
+// back_button.drawButton(bracket_open,container3);
+// space++;
+// const bracket_close_btn= new Button(10*space,10,40,40,"#9933CC");
+// back_button.drawButton(bracket_close,container3);
+// space++;
+// const percent_btn= new Button(10*space,10,40,40,"#9933CC");
+// back_button.drawButton(percent,container3);
+// space++;
+// const equals_btn= new Button(10*space,10,40,40,"#9933CC");
+// back_button.drawButton(equals,container3);
+// space=0;
+
+// const add_btn= new Button(10*space,10,40,40,"#9933CC");
+// back_button.drawButton(add,container4);
+// space++;
+
+// const minus_btn= new Button(10*space,10,40,40,"#9933CC");
+// back_button.drawButton(minus,container4);
+// space++;
+
+// const multiply_btn=new Button(10*space,10,40,40,"#9933CC");
+// back_button.drawButton(minus,container4);
+// space++;
+
+// const point_btn=new Button(10*space,10,40,40,"#9933CC");
+// back_button.drawButton(point,container4);
+// space++;
+// for(let j=5;j<10;j++){
+//     const b= new Button(10*space,10,40,40,'#9933CC');
+//     b.drawButton(""+j,container2);
+//     space++;
+// }
+
+
+
+
+
+
+
+
 
